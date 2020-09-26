29790a41b162ffcd04a51d1bcdca237b86a9a51d
250399 <kunaldeshmukh2503@gmail.com>
2020-09-26 17:41:21 +0530

    [Kunal] ADD: Characteristics of FP

--- functional.js
+++ functional.js
@@ -2,6 +2,7 @@
 // just like OOP. It is just decomposing of problems into small
 // and reuseable functions that take some input and returns result.
 
+
 // Characteristics of FP
 // Functions should be first class citizens i.e functions 
 // should be objects and hence can be passed as agrument to another functin
@@ -16,41 +17,3 @@
 // Immutable by FB, Immer, etc. Basic JS like spread operator, 
 // array method like filter, map, reduce, slice, etc can also be used.
 // 
-
-
-//Advantages
-// More predictable
-// Easier to test and debug
-// more scalable
-
-// Function as First Class Citizen example
-
-// Assigning function to variable
-function sayHello(){
-    return "Hello World"
-}
-
-let fun = sayHello // Assignning function to var
-console.log(fun()) // Callin fucntion through var
-
-// Passing funtion as paramter to another funtion
-// This actually can be very powerful. This actually opens up for
-// many cool things like decorators, Currying, etc.
-function greet(otherFunc){
-    console.log(otherFunc())
-}
-
-
-// Trim the input and add inside of div tags
-// 
-
-
-const trimInput = str => str.trim();
-const wrapInDiv = str => `<div>${str}</div>`
-
-const result = wrapInDiv(trimInput("     value        "));
-
-console.log(result)
-
-
-
